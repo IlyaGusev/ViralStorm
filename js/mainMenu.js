ba0b7c00406207f0a93ba7c26bd8c7e3dc705a31
WokinShopash <wokin.shopash@gmail.com>
2013-08-05 14:40:39 +0400

    добавил меню с хелпом и авторами

---
+++ js/mainMenu.js
@@ -0,0 +1,48 @@
+var bStart = document.getElementById('start');
+var bAuthors = document.getElementById('authors');
+var bHelp = document.getElementById('help');
+var bAuthorsOK = document.getElementById('authors-OK');
+var bHelpOK = document.getElementById('help-OK');
+
+var show_authors = function() {
+	document.getElementById ("authors-screen").style.display = 'block';
+}
+
+var hide_authors = function() {
+	document.getElementById ("authors-screen").style.display = 'none';
+}
+
+var show_help = function() {
+	document.getElementById ("help-screen").style.display = 'block';
+}
+
+var hide_authors = function() {
+	document.getElementById ("help-screen").style.display = 'none';
+}
+
+if (bStart.addEventListener)
+	bStart.addEventListener ("click", start, false);
+else if (bStart.attachEvent)
+	bStart.attachEvent ("onclick", start);
+
+if (bAuthors.addEventListener) 
+	bAuthors.addEventListener ("click", show_authors, false);
+else if (bAuthors.attachEvent)
+	bAuthors.attachEvent ("onclick", show_authors);
+
+if (bHelp.addEventListener)
+	bHelp.addEventListener ("click", show_help, false);
+else if (bHelp.attachEvent)
+	bHelp.attachEvent ("onclick", show_help);
+
+if (bHelpOK.addEventListener)
+	bHelpOK.addEventListener ("click", hide_help, false);
+else if (bHelpOK.attachEvent)
+	bHelpOK.attachEvent ("onclick", hide_help);
+
+if (bAuthorsOK.addEventListener)
+	bAuthorsOK.addEventListener ("click", hide_authors, false);
+else if (bAuthorsOK.attachEvent)
+	bAuthorsOK.attachEvent ("onclick", hide_authors);
+
+
